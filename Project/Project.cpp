8227c9be8b00fa8ab53665aefa713868f819940e
U-RavenSouls\RavenSoul <RavenSoul@RavenSouls.(none)>
2014-04-06 15:50:42 +0200

    first commit : Empty project (build files)

---
+++ Project/Project.cpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2014 - LGG EPFL
+
+#include <iostream>
+#include <sstream>
+#include "common.h"
+
+#include "vshader1.h"
+#include "gshader1.h"
+#include "fshader1.h"
+#include "vshader2.h"
+#include "fshader2.h"
+#include "gshader2.h"
+
+using namespace std;
+using namespace opengp;
+
+
+int main(int, char**) {
+    cout << "Hello World!\n";
+    return 0;    
+}
